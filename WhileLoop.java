b670869c770bfafaf4e2ff2480ed1f544977acc6
Ritikajaglan <88761335+Ritikajaglan@users.noreply.github.com>
2021-08-11 10:52:28 +0530

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+class WhileLoop
+ {
+  public static void main (String[] s)
+    {
+      int n=1;
+      while (n<11)
+        {
+         System.out.println(n);
+         n++;
+        }
+    }
+  }
