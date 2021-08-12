5b090474c6265e44f1403e4b4a90bc25bc11977f
Ritikajaglan <88761335+Ritikajaglan@users.noreply.github.com>
2021-08-12 12:32:08 +0530

    Create WhileloopScanner.java

---
+++ WhileloopScanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class WhileloopScanner
+{
+public static void main(String[] s)
+{
+System.out.println("Enter a Number")
+int b=1;
+Scanner sc=new Scanner(System. in);
+int num=sc.nextInt();
+while(b<11)
+{
+System.out.println(num+"×"+b+"="+num×b);
+b++;
+}
+}
+}
