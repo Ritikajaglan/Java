62e87ef99d2e581cc728b0bdc0da57ceb5416aea
Ritikajaglan <88761335+Ritikajaglan@users.noreply.github.com>
2021-08-14 13:34:45 +0530

    Create Thecube.java

---
+++ Thecube.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Thecube
+{
+public static void main(String[] s)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a Number");
+int num=sc.nextInt();
+System.out.println("The cube is"+num×num×num);
+System.out.println("The square is"+num×num);
+}
+}
