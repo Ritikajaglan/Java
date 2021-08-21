8fdc05073e5d5ae43f54541231d18ce8fd0f2517
Ritikajaglan <88761335+Ritikajaglan@users.noreply.github.com>
2021-08-22 00:18:07 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Matrix 
+{
+	public static void main(String[] s)
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a number");
+   int num=sc.nextInt();
+ 	int c=1;
+   for(int a=0;a<num;a++)
+ 	{
+	for(int b=0;b<num;b++)
+	{
+	System.out.print(c + " ");
+}
+System.out.println();
+  }
+ }
+}
+      			
